8bd75f7fbabf6acd815cb9a5fb4051c574e56f96
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-07 12:03:52 +0300

    creating components

--- Quotes-APP/src/app/app.component.ts
+++ Quotes-APP/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  quotes :Quotes[] =[
+    {id:1, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+    {id:2, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+    {id:3, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+    {id:4, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+    {id:5, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+    {id:6, name: 'You may not control all the events that happen to you, but you can decide not to be reduced by them.'},
+  ]
   
 }
