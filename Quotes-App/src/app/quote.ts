01c5a30f57c9c41a92f3a3fb0e60908631394cc4
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-07 12:43:01 +0300

    defining the datatype & setting strict iniatization of property to false

--- Quotes-App/src/app/quote.ts
+++ Quotes-App/src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    showInfo: boolean;
+    constructor(public id:number,public name:string,public quotes:string,public author:string,public likes:number,public dislikes:number){
+        this.showInfo=false
+    }
+
 }
