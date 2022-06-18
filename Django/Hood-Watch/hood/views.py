72358288757c8c6c31c2d296ed0d1a9e260441c7
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-18 13:35:24 +0300

    updating the function view

--- Django/Hood-Watch/hood/views.py
+++ Django/Hood-Watch/hood/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django .forms import SignUpForm,BusinessForm,UpdateProfileForm,NeighbourHoodForm,PostForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood,Profile,Business,Post
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -6,27 +11,48 @@ from django.shortcuts import render,redirect,get_object_or_404
 def index (request):    
     return render(request, 'index.html')
 
-def signup (request): 
+def signup(request):
     if request.method == 'POST':
-      return redirect('index')
-    return render(request, 'registrationsignup.html')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 def hoodsView (request):
     all_hoods =  Neighborhood.objects.all()
     all_hoods = all_hoods[::-1]
+    context = {
+        'all_hoods': all_hoods
+    }
     
-    return render(request, 'all_hoods.html')
+    return render(request, 'all_hoods.html',context)
 
 def newHood (request):
-    if request.method == 'POST':    
-        return redirect('hood')
-    return render(request, 'new_hood.html')
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
+
+   
 
 
 def hoodMembership (request,hood_id):   
     hood = Neighborhood.objects.get(id=hood_id)
     members =Profile.objects.filter(neighbourhood=hood).all()
-    return render(request, 'members.html')
+    return render(request, 'members.html',{'members':members})
 
 def join_hood(request,id):
     neighbourhood =get_object_or_404(neighbourhood,id=id)
@@ -45,19 +71,42 @@ def edit_profile(request,username):
 
 
 def add_amenity(request,hood_id):
-    hood =  Neighborhood.objects.get(id=hood_id)
+    hood = NeighbourHood.objects.get(id=hood_id)
     business = Business.objects.filter(neighbourhood=hood)
     posts = Post.objects.filter(hood=hood)
     posts = posts[::-1]
     if request.method == 'POST':
-        return redirect('add_amenity',hood.id)
-    return render(request, 'add_amenity.html')
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            b_form = form.save(commit=False)
+            b_form.neighbourhood = hood
+            b_form.user = request.user.profile
+            b_form.save()
+            return redirect('add_amenity', hood.id)
+    else:
+        form = BusinessForm()
+    params = {
+        'hood': hood,
+        'business': business,
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'add_amenity.html', params)
 
 def create_post(request,hood_id):
-    hood =  Neighborhood.objects.get(id=hood_id)    
+    hood = NeighbourHood.objects.get(id=hood_id)
     if request.method == 'POST':
-        return redirect('add_amenity',hood.id)
-    return render(request, 'post.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('add_amenity', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {'form': form})
+
 
 
 def search_business(request):
