519f683e9f747d0a5b6dad5c934fea1b4aeca6a3
Oscar Kamau <99794148+KamauDev-maker@users.noreply.github.com>
2022-06-18 14:13:45 +0300

    Delete Quotes-App directory

--- Quotes-App/src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    showInfo: boolean;
-    constructor(public id:number,public name:string,public quotes:string,public author:string,public likes:number,public dislikes:number){
-        this.showInfo=false
-    }
-
-}
